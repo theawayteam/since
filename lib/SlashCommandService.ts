183d58fba6c2aceb04578454f8a30a00adfae222
TheConnMan <bcconn2112@gmail.com>
2018-03-11 16:26:01 -0400

    Add the list and reset slash commands

--- lib/SlashCommandService.ts
+++ lib/SlashCommandService.ts
@@ -1,14 +1,41 @@
+import * as moment from 'moment';
+import Item from '../model/Item';
+import ItemService from './ItemService';
+
 export default class SlashCommandService {
 
-  public async process(msg, bot) {
-    const text = await this.getResponse(msg);
-    bot.replyPrivate(text);
-  }
+  private static HELP_TEXT = [
+    'Since Help',
+    ' - list: List current events',
+    ' - reset <event name>: Reset the time on an event'
+  ].join('\n');
 
-  private async getResponse(msg): Promise<string> {
-    return [
-      'Since Help',
-      ' - list: List current events'
-    ].join('\n');
+  private itemService = new ItemService();
+
+  public async process(msg, bot) {
+    if (!msg.text.trim()) {
+      return bot.replyPrivate(SlashCommandService.HELP_TEXT);
+    }
+    const parts = msg.text.trim().split(' ');
+    const command = parts[0];
+    if (command === 'list') {
+      const items = await this.itemService.list(msg.team_id);
+      const message = 'Events:\n' + items.sort((i1, i2) => {
+        return i2.timestamp - i1.timestamp;
+      }).map((item) => {
+        return `${item.name} - ${moment.duration(new Date().getTime() - item.timestamp, 'millisecond').humanize()} ago`;
+      }).join('\n');
+      bot.replyPrivate(message);
+    } else if (command === 'reset') {
+      const name = parts.slice(1).join(' ');
+      let item = await this.itemService.get(name, msg.team_id);
+      if (!item) {
+        item = new Item(msg.team_id, name, new Date().getTime());
+      } else {
+        item.timestamp = new Date().getTime();
+      }
+      this.itemService.save(item);
+      bot.replyPrivate(`Event ${name} saved`);
+    }
   }
 }
