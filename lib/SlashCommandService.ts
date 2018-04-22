82c1775d311101fe68ecae0749aede15f92a3dac
TheConnMan <bcconn2112@gmail.com>
2018-04-22 15:11:45 -0400

    Add debug messages for normal operations

--- lib/SlashCommandService.ts
+++ lib/SlashCommandService.ts
@@ -25,15 +25,18 @@ export default class SlashCommandService {
       const parts = msg.text.trim().split(' ');
       switch (parts[0]) {
         case 'list':
+          winston.debug(`User ${msg.user_name} in team ${msg.team_id} ran a list command`);
           const items = await this.itemService.list(msg.team_id);
           const message = this.messageService.generateListMessage(items);
           return bot.replyPrivate(message);
         case 'create':
+          winston.debug(`User ${msg.user_name} in team ${msg.team_id} ran a create command`);
           const name = parts.slice(1).join(' ');
           const item = new Item(msg.team_id, name, msg.user_name, new Date().getTime());
           this.itemService.save(item);
           return bot.replyPrivate(`:heavy_check_mark: Event ${name} saved`);
         default:
+          winston.debug(`User ${msg.user_name} in team ${msg.team_id} ran an other command`);
           return bot.replyPrivate(SlashCommandService.HELP_TEXT_OBJECT);
       }
     } catch (e) {
@@ -50,12 +53,14 @@ export default class SlashCommandService {
       const lastTime = item.timestamp;
       switch (action.value) {
         case 'reset':
+          winston.debug(`User ${msg.user.name} in team ${msg.team.id} reset an event`);
           item.timestamp = new Date().getTime();
           item.user = msg.user.id;
           await this.itemService.save(item);
           await bot.say(this.messageService.generateResetMessage(item, lastTime));
           break;
         case 'delete':
+          winston.debug(`User ${msg.user.name} in team ${msg.team.id} deleted an event`);
           await this.itemService.delete(item.id, item.teamId);
           break;
       }
